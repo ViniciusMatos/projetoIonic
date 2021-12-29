f0141bbc410e5ac4eea2163a0ab40d7b36e86eb7
ViniciusMatos <vmatos12@gmail.com>
2021-12-29 15:18:40 -0300

    Comit inicial - Criação do projeto

---
+++ src/test/java/br/com/oic/ProjetoIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
