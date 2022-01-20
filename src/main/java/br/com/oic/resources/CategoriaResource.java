98175b18e67eb37cd7fc7de1b1bacc8bb5e82dfe
ViniciusMatos <vmatos12@gmail.com>
2022-01-20 11:49:45 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/com/oic/resources/CategoriaResource.java
+++ src/main/java/br/com/oic/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package br.com.oic.resources;
  
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.oic.domain.Categoria;
 import br.com.oic.services.CategoriaService;
@@ -22,5 +26,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){ //anotation que converte json para objeto em java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();// pega a url utilizada para inserir  e acrescenta nela o id do objeto criado 
+		return ResponseEntity.created(uri).build();
 	}
 }
